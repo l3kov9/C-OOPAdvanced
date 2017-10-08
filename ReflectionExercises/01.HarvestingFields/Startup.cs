2f0b5782593c81e5426747791a69dba78d938e00
l3kov9 <l3kov.9@gmail.com>
2017-10-08 14:12:15 +0300

    Reflection update

---
+++ ReflectionExercises/01.HarvestingFields/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace _01.HarvestingFields
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var dictionary = new Dictionary<string, Func<FieldInfo[]>>
+                {
+                    { "private", ()=>typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public
+                |BindingFlags.NonPublic | BindingFlags.Static).Where(x=>x.IsPrivate).ToArray() },
+                     { "protected", ()=>typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public
+                |BindingFlags.NonPublic | BindingFlags.Static).Where(x=>x.IsFamily).ToArray() },
+                      { "public", ()=>typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public
+                |BindingFlags.NonPublic | BindingFlags.Static).Where(x=>x.IsPublic).ToArray() },
+                       { "all", ()=>typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public
+                |BindingFlags.NonPublic | BindingFlags.Static) }
+                };
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "HARVEST")
+                {
+                    break;
+                }
+
+                Print(dictionary[command]());
+
+                //switch (command)
+                //{
+                //    case "private": PrintAllPrivateFields();
+                //        break;
+                //    case "protected":
+                //        PrintAllProtectedFields();
+                //        break;
+                //    case "public":
+                //        PrintAllPublicFields();
+                //        break;
+                //    case "all":
+                //        PrintAllAllFields();
+                //        break;
+                //    default:
+                //        break;
+                //}
+            }
+        }
+
+        private static void PrintAllAllFields()
+        {
+            FieldInfo[] allFields = typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public
+                |BindingFlags.NonPublic | BindingFlags.Static);
+            Print(allFields);
+        }
+
+        private static void Print(FieldInfo[] array)
+        {
+            foreach (var field in array)
+            {
+                Console.WriteLine($"{field.Attributes} {field.FieldType.Name} {field.Name}"
+                    .Replace("Family","protected")
+                    .ToLower());
+            }
+        }
+
+        private static void PrintAllPublicFields()
+        {
+            FieldInfo[] publicFields = typeof(HarvestingFields).GetFields(BindingFlags.Instance | BindingFlags.Public);
+            Print(publicFields);
+        }
+
+        private static void PrintAllProtectedFields()
+        {
+            FieldInfo[] protectedFields = typeof(HarvestingFields).GetFields(BindingFlags.Instance 
+                | BindingFlags.NonPublic|BindingFlags.Public | BindingFlags.Static)
+                .Where(x=>x.IsFamily).ToArray();
+            Print(protectedFields);
+        }
+
+        private static void PrintAllPrivateFields()
+        {
+            FieldInfo[] privateFields = typeof(HarvestingFields).GetFields(BindingFlags.Instance
+                | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.IsPrivate).ToArray();
+            Print(privateFields);
+        }
+    }
+}
---
+++ ReflectionExercises/02.BlackBoxInteger/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace _02.BlackBoxInteger
+{
+    public class Startup
+    {
+        private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.NonPublic;
+
+        public static void Main()
+        {
+            Type blackboxType = typeof(BlackBoxInt);
+            BlackBoxInt blackboxInstance = (BlackBoxInt)Activator.CreateInstance(blackboxType, true);
+            Console.WriteLine(blackboxInstance.GetType());
+
+            //ConstructorInfo constructorInfo = blackBoxType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic
+            //    ,Type.DefaultBinder,new Type[] { }, null);
+            //constructorInfo.Invoke
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "END") break;
+
+                var methodTokens = input.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                var methodName = methodTokens[0];
+                var methodParam = int.Parse(methodTokens[1]);
+
+                blackboxType
+                    .GetMethod(methodName, Flags)
+                    .Invoke(blackboxInstance, new object[] { methodParam });
+
+                var innerValue = blackboxType
+                    .GetFields(Flags)
+                    .First()
+                    .GetValue(blackboxInstance);
+
+                Console.WriteLine(innerValue);
+            }
+        }
+    }
+}
